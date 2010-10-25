4ff366afad4ee001fe21ca2c74a61c645e8da676
ChrisWiegman <ChrisWiegman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-25 00:43:11 +0000

    ALPHA 2 commit
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/trunk@303809 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Better WP Security
 Plugin URI: http://www.chriswiegman.com/projects/wordpress/better-wp-security/
 Description: A collection of numerous security fixes and modifications to help protect a standard wordpress installation.
-Version: 0.1 ALPHA
+Version: ALPHA 2
 Author: ChrisWiegman
 Author URI: http://www.chriswiegman.com
 Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
