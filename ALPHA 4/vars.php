cecb5101aa332fbfde43d62b3f87153a0b6ee11d
ChrisWiegman <ChrisWiegman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-30 15:01:47 +0000

    Removed old tags
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/tags@305989 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ALPHA 4/vars.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $wpdb;
-
-$BWPS_limitlogin_table_hostfailstable = $wpdb->prefix . "BWPS_bad_host_attempts";
-$BWPS_limitlogin_table_userfailstable = $wpdb->prefix . "BWPS_bad_user_attempts";
-$BWPS_limitlogin_table_lockouthosttable = $wpdb->prefix . "BWPS_lockouthost";
-$BWPS_limitlogin_table_lockoutusertable = $wpdb->prefix . "BWPS_lockoutuser";
-
-$BWPS_currentVersion = 'alpha4'; //Define current version variable
