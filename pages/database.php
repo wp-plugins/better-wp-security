19a254b2fde863fcc564916f06d44e748e5a3f73
Bit51 <Bit51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-13 14:22:02 +0000

    Version 2.7.1
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/tags/2.7.1@437471 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pages/database.php
@@ -0,0 +1,219 @@
+<?php
+	global $wpdb, $BWPS;
+	
+	if (is_multisite()) {
+		$dpre = substr($wpdb->prefix,0,strpos($wpdb->prefix,'_') + 1);
+	} else {
+		$dpre =  $wpdb->prefix;
+	}
+	
+	if (isset($_POST['BWPS_database_save'])) {
+		
+		if (!wp_verify_nonce($_POST['wp_nonce'], 'BWPS_database_save')) {
+			die('Security error!');
+		}
+		
+		 $checkPreExists = $dpre;
+		
+		while ($checkPreExists) {
+			$prelength = rand(3,5);
+			$newPrefix = substr(md5(rand()), rand(0, (32 - $prelength)), $prelength) . '_';
+			$checkPreExists = $wpdb->get_results('SHOW TABLES LIKE "' . $newPrefix . '%";', ARRAY_N);
+		}
+		
+		$tables = $wpdb->get_results('SHOW TABLES LIKE "' . $dpre . '%"', ARRAY_N);
+					
+		if ($tables) {
+			$tablesCopied = array();
+						
+			foreach ($tables as $table) {
+				$table = substr($table[0], strlen($dpre), strlen($table[0]));
+							
+				$sql = 'CREATE TABLE `' . $newPrefix . $table . '`LIKE `' . $dpre . $table . '`;';
+					
+				$createTable = $wpdb->query($sql);
+			
+				if ($createTable === false) {
+					if (!$errorHandler) {
+						$errorHandler = new WP_Error();
+					}
+			
+					$errorHandler->add("2", __("Could not create table.", 'better-wp-security'));
+				} else {
+					
+					$sql = 'INSERT INTO `' . $newPrefix . $table . '` SELECT * FROM `' . $dpre . $table . '`;';
+								
+					$popTable = $wpdb->query($sql);
+								
+					if ($popTable === false) {
+						if (!$errorHandler) {
+							$errorHandler = new WP_Error();
+						}
+			
+						$errorHandler->add("2", __("Could not copy table.", 'better-wp-security'));
+					} else {
+							$tablesCopied[] = $table;
+					}
+				}
+			}
+				
+			if (count($tablesCopied) == count($tables)) {
+			
+				$upOpts = true;
+				if (is_multisite()) {
+					$blogs = get_blog_list( 0, 'all' ); //need to find a non-deprecated alternative
+					if( is_array( $blogs ) ) {
+						foreach( $blogs as $details ) {
+							$results = $wpdb->query('UPDATE ' . $newPrefix . $details['blog_id'] . '_options SET option_name = "' . $newPrefix . $details['blog_id'] . '_user_roles" WHERE option_name = "' . $dpre . $details['blog_id'] . '_user_roles" LIMIT 1;');
+							if ($results === false) {
+								$upOpts = false;
+							}
+						}
+					}
+				} else {
+					$upOpts = $wpdb->query('UPDATE ' . $newPrefix . 'options SET option_name = "' . $newPrefix . 'user_roles" WHERE option_name = "' . $dpre . 'user_roles" LIMIT 1;');
+				}
+							
+				if ($upOpts === false && !is_multisite()) {
+					if (!$errorHandler) {
+						$errorHandler = new WP_Error();
+					}
+			
+					$errorHandler->add("2", __("Could not update prefix refences in options table.", 'better-wp-security'));
+				}
+					
+				$rows = $wpdb->get_results('SELECT * FROM ' . $newPrefix . 'usermeta');
+				$upMeta = true;
+				foreach ($rows as $row) {
+					if (substr($row->meta_key,0,strlen($dpre)) == $dpre) {
+						$pos = $newPrefix . substr($row->meta_key, strlen($dpre), strlen($row->meta_key));
+						$result = $wpdb->query('UPDATE ' . $newPrefix . 'usermeta SET meta_key="' . $pos . '" WHERE meta_key= "' . $row->meta_key . '" LIMIT 1;');
+					}
+					if ($result == false) {
+						$upMeta = false;
+					}
+				}
+
+				if ($upMetar === FALSE) {
+					if (!$errorHandler) {
+						$errorHandler = new WP_Error();
+					}
+			
+					$errorHandler->add("2", __("Could not update prefix refences in usermeta table.", 'better-wp-security'));
+				}
+			}
+		}
+		
+		$tables = $wpdb->get_results('SHOW TABLES LIKE "' . $dpre . '%"', ARRAY_N);
+					
+		if ($tables) {
+			$tablesDropped = array();
+						
+			foreach ($tables as $table) {
+				$table = $table[0];
+							
+				$dropTable = $wpdb->query('DROP TABLE `' . $table . '`;');
+							
+				if ($dropTable === false) {
+					if (!$errorHandler) {
+						$errorHandler = new WP_Error();
+					}
+					$errorHandler->add("2", __("Could not drop table.", 'better-wp-security'));
+				} else {
+					$tablesDropped[] = $table;
+				}
+			}
+		}
+		
+		$conf_f = $BWPS->getConfig();
+
+		chmod($conf_f, 0755);
+		$handle = @fopen($conf_f, "r+");
+		if ($handle) {
+			while (!feof($handle)) {
+				$lines[] = fgets($handle, 4096);
+			}
+			fclose($handle);
+			$handle = @fopen($conf_f, "w+");
+			foreach ($lines as $line) {
+				if (strpos($line, $dpre)) {
+					$line = str_replace($dpre, $newPrefix, $line);
+				}
+				fwrite($handle, $line);
+			}
+			fclose($handle);
+		}
+        	
+		if (isset($errorHandler)) {
+			echo '<div id="message" class="error"><p>' . $errorHandler->get_error_message() . '</p></div>';
+		} else {
+			echo '<div id="message" class="updated"><p>' . __('Database Prefix Changed.', 'better-wp-security') . '</p></div>';
+		}
+	}
+	
+	function checkTablePre(){
+		global $wpdb;
+		
+		if ($dpre == 'wp_') {
+			return true;
+		}else{
+			echo false;
+		}
+	}
+		
+?>
+
+<div class="wrap" >
+
+	<h2>Better WP Security - <?php _e('Database Prefix', 'better-wp-security'); ?></h2>
+	
+	<div id="poststuff" class="ui-sortable">
+	
+		<?php if ($BWPS->can_write($BWPS->getConfig())) { ?>
+			<?php 
+				if ((checkTablePre() && !isset($_POST['BWPS_database_save'])) || (!checkTablePre() && isset($_POST['BWPS_database_save']) && isset($errorHandler))) {
+					$bgcolor = "#ffebeb";
+				} else {
+					$bgcolor = "#fff";
+				}
+			?>
+			<div class="postbox-container" style="width:70%">	
+				<div class="postbox opened" style="background-color: <?php echo $bgcolor; ?>;">
+					<h3><?php _e('Database Prefix', 'better-wp-security'); ?></h3>	
+					<div class="inside">
+					<p><?php _e('Use the form below to change the table prefix for your Wordpress Database.', 'better-wp-security'); ?></p>
+					<p style="text-align: center; font-size: 130%; font-weight: bold; color: blue;"><?php _e('WARNING: BACKUP YOUR DATABASE BEFORE USING THIS TOOL!', 'better-wp-security'); ?></p>
+						<?php if ((checkTablePre() && !isset($_POST['BWPS_database_save'])) || (!checkTablePre() && isset($_POST['BWPS_database_save']) && isset($errorHandler))) { ?>
+							<p><strong><?php _e('Your database is using the default table prefix', 'better-wp-security'); ?> <em>wp_</em>. <?php _e('You should change this.', 'better-wp-security'); ?></strong></p>
+						<?php } else { ?>
+							<?php 
+								if (isset($_POST['BWPS_database_save']) && !isset($errorHandler)) {
+									$pre = $newPrefix;
+								} else {
+									$pre = $dpre;
+								}
+							?>
+							<p><?php _e('Your current database table prefix is', 'better-wp-security'); ?> <strong><em><?php echo $pre; ?></em></strong></p>
+						<?php } ?>
+						<form method="post">
+							<?php wp_nonce_field('BWPS_database_save','wp_nonce') ?>
+							<p><?php _e('Press the button below to generate a random database prefix value and update all of your tables accordingly.', 'better-wp-security'); ?></p>
+							<p class="submit"><input type="submit" name="BWPS_database_save" value="<?php _e('Change Database Table Prefix', 'better-wp-security'); ?>"></p>
+						</form>
+					</div>
+				</div>
+			</div>
+		<?php } else { ?>
+			<div class="postbox-container" style="width:70%">	
+				<div class="postbox opened" style="background-color: #ffebeb;">
+					<h3><?php _e('Database Prefix', 'better-wp-security'); ?></h3>	
+					<div class="inside">
+						<h4><?php _e('Warning: Better WP Security cannot write to your <em>wp-config.php</em> file. You must fix this error before continuing.','better-wp-security'); ?></h4>
+					</div>
+				</div>
+			</div>
+		<?php } ?>		
+		<?php include_once(trailingslashit(WP_PLUGIN_DIR) . 'better-wp-security/pages/donate.php'); ?>
+		
+	</div>
+</div>
---
+++ readme.txt
@@ -0,0 +1,90 @@
+
+=== Better WP Security ===
+Contributors: Bit51
+Donate link: http://bit51.com/software/better-wp-security/
+Tags: security, secure, multi-site, network, mu, login, lockdown, htaccess, hack, header, cleanup, ban, restrict, access, protect, protection, disable, images, image, hotlink, admin, username, database, prefix, wp-content, rename, directory, directories, secure, SSL
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 2.7.1
+
+Helps secure Wordpress by protecting your single or multi-site installation from attackers. Hardens standard Wordpress security by hiding vital areas of your site, protecting access to important files via htaccess, preventing brute-force login attempts, detecting attack attempts, and more.
+
+== License ==  
+Released under the terms of the GNU General Public License. 
+
+== Description ==
+
+\#1 WORDPRESS SECURITY PLUGIN
+
+Better WP Security takes the best Wordpress security features and techniques and combines them in a single plugin thereby ensuring that as many security holes as possible are patched without  having to worry about conflicting features or the possibility of missing anything on your site.
+
+= Current features =
+
+1. Remove the meta "Generator" tag
+2. Removes login error messages
+3. Change the urls for backend functions including login, admin, and more
+4. Limit admin access to specified IP or range of IP addresses
+5. Ban troublesome bots and other hosts
+6. Completely turn off the ability to login for a given time period (away mode)
+7. Prevent brute force attacks by banning hosts and users with too many invalid login attempts
+8. Display a random version number to non administrative users anywhere version is used (often attached to plugin resources such as scripts and style sheets)
+9. Remove theme, plugin, and core update notifications from users who do not have permission to update them (useful on multisite installations)
+10. Remove Windows Live Write header information
+11. Remove RSD header information
+12. Strengthen .htaccess settings
+13. Enforce strong passwords for all accounts of a configurable minimum role
+14. Detect attempts to attack your site
+15. Rename "admin" account
+16. Security checker
+17. Change the Wordpress database table prefix
+18. Force SSL for admin pages (on supporting servers)
+19. Change wp-content path
+20. Turn off file editing from within Wordpress admin area
+20. Works on multi-site (network) and single site installations
+
+= Translations =
+* Bahasa Indonesia (Indonesian) by <a href="http://dhany.web.id/panduan-seo">Belajar SEO, Jasa SEO Indonesia</a>
+* French by Claude ALTAYRAC
+* Italian by <a href="http://www.polslinux.it">Paolo Stivanin</a>
+
+= More Information =
+* <a href="http://bit51.com/2011/09/fixing-better-wp-security-lockouts/">Fixing Better WP Security Lockouts</a>
+* <a href="http://bit51.com/2011/09/what-is-changed-by-better-wp-security/">What is Changed By Better WP Security</a>
+
+== Installation ==
+
+1. Backup your Wordpress database, config file, and .htaccess file
+2. Upload the zip file to the `/wp-content/plugins/` directory
+3. Unzip
+4. Activate the plugin through the 'Plugins' menu in WordPress
+5. Visit the Better security menu for checklist and options
+
+NOTE: It is quite possible (maybe even probable) that something will break due to the complexity of the changes made by this plugin. That said, under no circumstances do I release this plugin with any warranty, implied or otherwise, and at no time will I take any responsibility for any damage that might arise from the use of this plugin. REMEMBER TO ALWAYS BACKUP BEFORE TRYING NEW SOFTWARE!
+
+== Frequently Asked Questions ==
+
+= Are you still developing this plugin? =
+* Yes. The functionality of this plugin is a requirement of my job so this plugin will continue to be developed.
+
+= Does this work with netowork or multisite installations? =
+* Yes, as of version 1.7 it should work fine with multisite installations.
+
+= Can I help? =
+* Of course! I am in constant need of testers and I would be more than happy to add the right contributor. In addition, I could always use help with translations for internationalization.
+
+= Will this break my site? =
+* It is quite possible (maybe even probable) that something will break. That said, under no circumstances do I release this plugin with any warranty, implied or otherwise, and at no time will I take any responsibility for any damage that might arise from the use of this plugin. REMEMBER TO ALWAYS BACKUP BEFORE TRYING NEW SOFTWARE!
+* Note that renaming the wp-content directory will not update the path in existing content. Use this feature only on new sites or in a situation where you can easily update all existing links.
+
+= I've enabled the Enforce SSL option and it broke my site. How do I get back in? =
+* Open your wp-config.php file in a text editor and remove the following 2 lines:
+* define('FORCE_SSL_LOGIN', true);;
+* define('FORCE_SSL_ADMIN', true);;
+
+== Changelog ==
+
+<a href="http://bit51.com/software/better-wp-security/changelog/">Click here for the full change log</a>
+
+== Support ==
+
+Please visit the <a href="http://bit51.com/software/better-wp-security/">Homepage</a> for support and more
