27b01da06658ef4dbc2e65db79b0ef268760ba0d
Bit51 <Bit51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-23 16:06:35 +0000

    Major refactoring
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/trunk@509421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/backend.php
+++ inc/backend.php
@@ -1,107 +1,125 @@
 <?php
 
-if (!class_exists('bwps_backend')) {
+if ( ! class_exists( 'bwps_backend' ) ) {
 
 	class bwps_backend extends bwps_admin {
 	
 		function __construct() {
 						
-			if (is_admin() || (is_multisite() && is_network_admin())) {
+			if ( is_admin() || (is_multisite() && is_network_admin() ) ) {
 			
 				//add scripts and css
-				add_action('admin_print_scripts', array(&$this, 'config_page_scripts'));
-				add_action('admin_print_styles', array(&$this, 'config_page_styles'));
+				add_action( 'admin_print_scripts', array( &$this, 'config_page_scripts' ) );
+				add_action( 'admin_print_styles', array( &$this, 'config_page_styles' ) );
 			
-				if (is_multisite()) { 
-					add_action('network_admin_menu', array(&$this, 'register_settings_page')); 
+				if ( is_multisite() ) { 
+					add_action( 'network_admin_menu', array( &$this, 'register_settings_page' ) ); 
 				} else {
-					add_action('admin_menu',  array(&$this, 'register_settings_page'));
+					add_action( 'admin_menu',  array( &$this, 'register_settings_page' ) );
 				}
 			
 				//add settings
-				add_action('admin_init', array(&$this, 'register_settings'));
+				add_action( 'admin_init', array( &$this, 'register_settings' ) );
 			
 				//add action link
-				add_filter('plugin_action_links', array(&$this, 'add_action_link'), 10, 2);
+				add_filter( 'plugin_action_links', array( &$this, 'add_action_link' ), 10, 2 );
 			
 				//add donation reminder
-				add_action('admin_init', array(&$this, 'ask'));	
+				add_action( 'admin_init', array( &$this, 'ask' ) );	
 			
-				if (isset($_POST['bwps_page'])) {
-					add_action('admin_init', array(&$this, 'form_dispatcher'));
+				if (isset( $_POST['bwps_page']) ) {
+					add_action( 'admin_init', array( &$this, 'form_dispatcher' ) );
 				}
 				
-				add_action('admin_init', array(&$this, 'awaycheck'));
+				add_action( 'admin_init', array( &$this, 'awaycheck' ) );
 			}
 			
-			add_action('init', array(&$this, 'backup_scheduler'));
+			add_action( 'init', array( &$this, 'backup_scheduler' ) );
 						
 		}
 		
 		function awaycheck() {
+		
 			global $bwps;
 			
-			if($bwps->checkaway()) {
-				wp_redirect(get_option('siteurl'));
+			if( $bwps->checkaway() ) {
+				wp_redirect( get_option( 'siteurl' ) );
 			}
+			
 		}
 		
 		function backup_scheduler() {
 		
-			add_action('bwps_backup', array(&$this, 'db_backup'));
+			add_action( 'bwps_backup', array( &$this, 'db_backup' ) );
 			
-			$options = get_option($this->primarysettings);
+			$options = get_option( $this->primarysettings );
 			
-			if ($options['backup_enabled'] == 1) {
-				if (!wp_next_scheduled('bwps_backup')) {
-					wp_schedule_event(time(), $options['backup_int'], 'bwps_backup');
+			if ( $options['backup_enabled'] == 1 ) {
+			
+				if ( ! wp_next_scheduled( 'bwps_backup' ) ) {
+					wp_schedule_event( time(), $options['backup_int'], 'bwps_backup' );
 				}
+				
 			} else {
-				if (wp_next_scheduled('bwps_backup')) {
-					wp_clear_scheduled_hook('bwps_backup');
+			
+				if ( wp_next_scheduled( 'bwps_backup' ) ) {
+					wp_clear_scheduled_hook( 'bwps_backup' );
 				}
+				
 			}
+			
 		}
 		
 		function db_backup() {
+		
 			global $wpdb;
 			$this->errorHandler = '';
 			
 			$backuppath = BWPS_PP . 'lib/phpmysqlautobackup/backups/';
 			
-			$options = get_option($this->primarysettings);
+			$options = get_option( $this->primarysettings );
 			
-			@require(BWPS_PP . 'lib/phpmysqlautobackup/run.php');
+			@require( BWPS_PP . 'lib/phpmysqlautobackup/run.php' );
 			
-			$wpdb->query('DROP TABLE `phpmysqlautobackup`;');
-			$wpdb->query('DROP TABLE `phpmysqlautobackup_log`;');
+			$wpdb->query( 'DROP TABLE `phpmysqlautobackup`;' );
+			$wpdb->query( 'DROP TABLE `phpmysqlautobackup_log`;' );
+			
 		}
 		
 		function getConfig() {
-			if (file_exists(trailingslashit(ABSPATH) . 'wp-config.php')) {
-				return trailingslashit(ABSPATH) . 'wp-config.php';
+		
+			if ( file_exists( trailingslashit( ABSPATH ) . 'wp-config.php' ) ) {
+			
+				return trailingslashit( ABSPATH ) . 'wp-config.php';
+				
 			} else {
-				return trailingslashit(dirname(ABSPATH)) . 'wp-config.php';
+			
+				return trailingslashit( dirname( ABSPATH ) ) . 'wp-config.php';
+				
 			}
+			
 		}
 		
-		function user_exists($username) {
+		function user_exists( $username) {
+		
 			global $wpdb;
 			
 			//return false if username is null
-			if ($username == '') {
+			if ( $username == '' ) {
 				return false;
 			}
 			
 			//queary the user table to see if the user is there
-			$user = $wpdb->get_var("SELECT user_login FROM `" . $wpdb->users . "` WHERE user_login='" . sanitize_text_field($username) . "'");
+			$user = $wpdb->get_var( "SELECT user_login FROM `" . $wpdb->users . "` WHERE user_login='" . sanitize_text_field( $username) . "';" );
 			
-			if ($user == $username) {
+			if ( $user == $username ) {
 				return true;
 			} else {
 				return false;
 			}
+			
 		}	
 		
 	}	
+	
 }
