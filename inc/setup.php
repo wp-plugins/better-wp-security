6449f7cae35464e03b5d4db8f5d5cff2a838eeef
Bit51 <Bit51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-09 01:32:29 +0000

    multisite fix for new upgrade mechanism
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/trunk@516538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/setup.php
+++ inc/setup.php
@@ -78,7 +78,7 @@ if ( ! class_exists( 'bwps_setup' ) ) {
 			global $wpdb;
 		
 			//if this is multisite make sure they're network activating or die
-			if ( is_multisite() && ! strpos( $_SERVER['REQUEST_URI'], 'wp-admin/network/plugins.php' ) ) {
+			if ( get_option( $this->plugindata ) === false && is_multisite() && ! strpos( $_SERVER['REQUEST_URI'], 'wp-admin/network/plugins.php' ) ) {
 			
 				die ( __( '<strong>ERROR</strong>: You must activate this plugin from the network dashboard.', $bwps->hook ) );	
 			
