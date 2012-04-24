f7af315a002b12ff1678b62bd0b606182a07dd9a
Bit51 <Bit51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-24 14:05:07 +0000

    updated language
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/trunk@535436 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin/content.php
+++ inc/admin/content.php
@@ -2196,7 +2196,7 @@ if ( ! class_exists( 'bwps_admin_content' ) ) {
 					?>
 					<tr valign="top" style="border: 1px solid #ff0000;">
 						<th scope="row">
-							<a name="ssl_frontend"></a><label for "ssl_frontend"><?php _e( 'Front end SSL', $this->hook ); ?></label>
+							<a name="ssl_frontend"></a><label for "ssl_frontend"><?php _e( 'Enforce Front end SSL', $this->hook ); ?></label>
 						</th>
 						<td>
 							<select id="ssl_frontend" name="ssl_frontend">
@@ -2204,7 +2204,7 @@ if ( ! class_exists( 'bwps_admin_content' ) ) {
 								<option value="1" <?php selected( $bwpsoptions['ssl_frontend'], '1' ); ?>><?php _e( 'Per Content', $this->hook ); ?></option>
 								<option value="2" <?php selected( $bwpsoptions['ssl_frontend'], '2' ); ?>><?php _e( 'Whole Site', $this->hook ); ?></option>
 							</select>
-							<p><?php _e( 'Enables secure SSL connection for the front-end (public parts of your site). Turning this off will disable front-end SSL, turning this on "Per Content" will place a checkbox on the edit page for all posts and pages (near the publish settings) allowing you turn to on SSL for selected pages or posts, and selecting "Whole Site" will force the whole site to use SSL (not recommended unless you have a really good reason to use it).', $this->hook ); ?></p>
+							<p><?php _e( 'Enables secure SSL connection for the front-end (public parts of your site). Turning this off will disable front-end SSL control, turning this on "Per Content" will place a checkbox on the edit page for all posts and pages (near the publish settings) allowing you turn to on SSL for selected pages or posts, and selecting "Whole Site" will force the whole site to use SSL (not recommended unless you have a really good reason to use it).', $this->hook ); ?></p>
 						</td>
 					</tr>
 					<tr valign="top" style="border: 1px solid #ff0000;">
