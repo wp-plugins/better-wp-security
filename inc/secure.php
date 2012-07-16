7d3b94f2e6a2e052c628f58c126051be8211653c
Bit51 <Bit51@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-16 19:38:05 +0000

    Better logic for SSL checking.
    git-svn-id: https://plugins.svn.wordpress.org/better-wp-security/trunk@573215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/secure.php
+++ inc/secure.php
@@ -314,7 +314,8 @@ if ( ! class_exists( 'bwps_secure' ) ) {
 		 **/
 		function checkssl() {
 			
-			if ( $_SERVER['SERVER_PORT'] == '443' ) {
+			//modified logic courtesy of "Good Samaritan"
+			if ( !empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443 ) {
 				return true;
 			} else {
 				return false;
